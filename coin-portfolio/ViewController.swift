72e90fa10f93503c5aecef48220cf0026e86ecbe
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-11-26 15:48:49 +0100

    Initial Commit

---
+++ coin-portfolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coin-portfolio
+//
+//  Created by Nikita on 26/11/2017.
+//  Copyright © 2017 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
